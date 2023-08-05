e6cb4a8797b16da04569d3bbdea378b429fbb534
Sirawit Pratoomsuwan <48322519+bronystylecrazy@users.noreply.github.com>
2023-08-06 01:04:23 +0700

    Initial commit

---
+++ essential-apps.sh
@@ -0,0 +1,69 @@
+brew tap iina/homebrew-mpv-iina
+brew install mpv-iina
+brew install tree
+brew install htop
+brew install wget
+brew install nvm
+brew install z
+brew install go
+brew install dockert
+brew install speedtest-cli
+brew install act
+brew install --cask arc
+brew install --cask redisinsight
+brew install --cask github
+brew install --cask visual-studio-code
+brew install --cask raycast
+brew install --cask vlc
+brew install --cask jetbrains-toolbox
+brew install --cask termius
+brew install --cask insomnia
+brew install --cask microsoft-teams
+brew install --cask telegram
+brew install --cask discord
+brew install --cask spotify
+brew install --cask rectangle
+brew install --cask betterdisplay
+brew install --cask keysmith
+brew install --cask slack
+brew install --cask gitkraken
+brew install --cask cinebench
+brew install --cask dbgate
+brew install --cask dbeaver-community
+brew install --cask canva
+brew install --cask shottr
+brew install --cask command-x
+brew install --cask aldente
+brew install --cask firefox
+brew install --cask anaconda
+brew install --cask google-chrome
+brew install --cask android-studio
+brew install --cask bilibili
+brew install --cask cloudflare-warp
+brew install --cask cyberduck
+brew install --cask dbngin
+brew install --cask dbngin
+brew install --cask hyperkey
+brew install --cask iterm2
+brew install --cask keyboard-cleaner
+brew install --cask messenger
+brew install --cask notion-enhanced
+brew install --cask openvpn-connect
+brew install --cask postman
+brew install --cask vscodium
+brew install --cask unity-hub
+brew install --cask warp
+brew install --cask wireshark
+brew install --cask zoom
+brew install --cask topnotch
+brew install --cask cheatsheet
+brew install --cask rocket
+
+# Goto https://handmirror.app/
+brew install --cask alt-tab
+brew install --cask selfcontrol
+brew install --cask imageoptim
+brew install --cask latest
+brew install mos
+brew install --cask background-music
+# Goto0 https://sindresorhus.com/speediness
---
+++ install-homebrew.sh
@@ -0,0 +1,5 @@
+echo "Install Homebrew..."
+echo "================================================"
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
